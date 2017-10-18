2bd0796004b19fe5e5485b1e96fe87d1d2a674b3
Christophe <christophe@tralalere.com>
2017-10-18 11:21:43 +0200

    Work in validators

--- src/validators/array-validator.class.ts
+++ src/validators/array-validator.class.ts
@@ -2,6 +2,7 @@
  * Created by Christophe on 18/10/2017.
  */
 import {Validator} from "./validator.class";
+import {ModelSchema} from "../model-schema.class";
 
 export class ArrayValidator extends Validator {
     
@@ -33,6 +34,46 @@ export class ArrayValidator extends Validator {
 
         this._stack.push((val:any[]) => {
             return val.length <= max;
+        });
+
+        return this;
+    }
+
+    string():ArrayValidator {
+        return this._isTypeOf("string");
+    }
+
+    number():ArrayValidator {
+        return this._isTypeOf("number");
+    }
+
+    schema(schema:ModelSchema):ArrayValidator {
+
+        this._stack.push((val:{[key:string]:any}[]) => {
+
+            for (let elem of val) {
+                if (!schema.validateModel(elem)) {
+                    return false;
+                }
+            }
+
+            return true;
+        });
+
+        return this;
+    }
+
+    private _isTypeOf(type:string):ArrayValidator {
+
+        this._stack.push((val:any[]) => {
+
+            for (let elem of val) {
+                if ((typeof elem) !== type) {
+                    return false;
+                }
+            }
+
+            return true;
         });
 
         return this;
--- src/validators/object-validator.class.ts
+++ src/validators/object-validator.class.ts
@@ -2,6 +2,7 @@
  * Created by Christophe on 18/10/2017.
  */
 import {Validator} from "./validator.class";
+import {ModelSchema} from "../model-schema.class";
 
 export class ObjectValidator extends Validator {
 
@@ -9,5 +10,28 @@ export class ObjectValidator extends Validator {
         func:Function
     ) {
         super(func);
+    }
+
+    schema(schema:ModelSchema):ObjectValidator {
+
+        this._stack.push((val:{[key:string]:any}) => {
+            return schema.validateModel(val);
+        });
+
+        return this;
+    }
+
+    keySchema(key:string, schema:ModelSchema):ObjectValidator {
+
+        this._stack.push((val:{[key:string]:any}) => {
+
+            if (!val[key]) {
+                return false;
+            }
+
+            return schema.validateModel(val[key]);
+        });
+
+        return this;
     }
 }
