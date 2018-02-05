16936e613e803de79cbb237e68b9e2b8d9dc2500
Christophe <christophe.vimbert@tralalere.com>
2018-02-05 14:52:53 +0100

    Export model class in index

--- index.ts
+++ index.ts
@@ -4,3 +4,4 @@
 export * from "./src/models-manager.class";
 export * from "./src/schema/model-schema.class";
 export * from "./src/structures/structures.class";
+export * from "./src/model/model.class";
